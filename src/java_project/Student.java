7b37f527bf6f0c592ec38e3009cea176f1010662
kimjaewon0418 <andy0418@naver.com>
2020-09-22 18:05:00 +0900

    create student class

--- src/java_project/Student.java
+++ src/java_project/Student.java
@@ -1,5 +1,13 @@
 package java_project;
 
 public class Student {
-
+	String name;
+	String dept;
+	public Student(String name, String dept, int grade) {
+		super();
+		this.name = name;
+		this.dept = dept;
+		this.grade = grade;
+	}
+	int grade;
 }
